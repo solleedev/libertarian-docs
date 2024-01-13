c8f718043d88103d612a3fde3558a923b603a84f
Sollee <solleedev@proton.me>
2024-01-13 15:59:54 +0000

    Set up Nix flakes

---
+++ flake.nix
@@ -0,0 +1,31 @@
+{
+  description = "Libertarian documentation";
+
+  inputs = {
+    flake-utils.url = "github:numtide/flake-utils";
+    nixpkgs.url = "nixpkgs/nixos-23.11";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+        };
+      in
+      rec {
+        flakedPkgs = pkgs;
+
+        # enables use of `nix shell`
+        devShell = pkgs.mkShell {
+          # add things you want in your shell here
+          buildInputs = with pkgs; [
+            nodejs
+            bun
+            jq
+            corepack
+          ];
+        };
+      }
+    );
+}
